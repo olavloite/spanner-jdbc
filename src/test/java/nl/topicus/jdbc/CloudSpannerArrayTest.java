a0fe9f49cb1529c768a344ecf5338314a24214c4
olavloite <koloite@gmail.com>
2017-09-02 20:55:50 +0200

    added tests

--- src/test/java/nl/topicus/jdbc/CloudSpannerArrayTest.java
+++ src/test/java/nl/topicus/jdbc/CloudSpannerArrayTest.java
@@ -2,6 +2,8 @@ package nl.topicus.jdbc;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
@@ -35,6 +37,20 @@ public class CloudSpannerArrayTest
 	}
 
 	@Test
+	public void testCreateArray1() throws SQLException
+	{
+		thrown.expect(SQLException.class);
+		CloudSpannerArray.createArray("Unknown type", new String[] { "foo", "bar" });
+	}
+
+	@Test
+	public void testCreateArray2() throws SQLException
+	{
+		thrown.expect(SQLException.class);
+		CloudSpannerArray.createArray("String", new Integer[] { 1, 2 });
+	}
+
+	@Test
 	public void testGetBaseTypeName() throws SQLException
 	{
 		for (CloudSpannerArray testSubject : testSubjects)
@@ -55,6 +71,9 @@ public class CloudSpannerArrayTest
 		{
 			assertArrayEquals(inputArray, (Object[]) testSubject.getArray());
 			assertArrayEquals(inputArray, (Object[]) testSubject.getArray(Collections.emptyMap()));
+			// Note that the getArray(long index, int count) is 1-based (first
+			// index is 1)
+			assertArrayEquals(Arrays.copyOfRange(inputArray, 0, 2), (Object[]) testSubject.getArray(1, 2));
 		}
 	}
 
@@ -84,6 +103,34 @@ public class CloudSpannerArrayTest
 	{
 		thrown.expect(SQLFeatureNotSupportedException.class);
 		testSubjects.get(0).getResultSet(0, 0, null);
+	}
+
+	@Test
+	public void testFree() throws SQLException
+	{
+		CloudSpannerArray array = CloudSpannerArray.createArray("String", inputArray);
+		assertArrayEquals(inputArray, (Object[]) array.getArray());
+		array.free();
+		thrown.expect(SQLException.class);
+		array.getArray();
+	}
+
+	@Test
+	public void testToString() throws SQLException
+	{
+		CloudSpannerArray array = CloudSpannerArray.createArray("String", inputArray);
+		assertEquals("{one,two,three}", array.toString());
+	}
+
+	@Test
+	public void testHashCodeAndEquals() throws SQLException
+	{
+		CloudSpannerArray array1 = CloudSpannerArray.createArray("String", inputArray);
+		CloudSpannerArray array2 = CloudSpannerArray.createArray("String", inputArray);
+		CloudSpannerArray array3 = CloudSpannerArray.createArray("String", new String[] { "foo", "bar" });
+		assertTrue(array1.equals(array2));
+		assertTrue(array1.hashCode() == array2.hashCode());
+		assertFalse(array1.equals(array3));
 	}
 
 }
