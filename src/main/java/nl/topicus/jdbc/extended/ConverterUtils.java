6795048fd9400f9b0d00c56e0f444c0ce6c7eb98
olavloite <koloite@gmail.com>
2017-09-25 22:01:06 +0200

    removed unused code

--- src/main/java/nl/topicus/jdbc/extended/ConverterUtils.java
+++ src/main/java/nl/topicus/jdbc/extended/ConverterUtils.java
@@ -4,11 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.google.cloud.ByteArray;
 
 public class ConverterUtils
 {
@@ -41,18 +38,6 @@ public class ConverterUtils
 		try (ResultSet indices = destination.getMetaData().getIndexInfo(catalog, schema, table, false, false))
 		{
 			while (indices.next())
-				count++;
-		}
-		return count;
-	}
-
-	public int getNumberOfColumns(Connection destination, String catalog, String schema, String table)
-			throws SQLException
-	{
-		int count = 0;
-		try (ResultSet cols = destination.getMetaData().getColumns(catalog, schema, table, null))
-		{
-			while (cols.next())
 				count++;
 		}
 		return count;
@@ -116,117 +101,6 @@ public class ConverterUtils
 			}
 		}
 		return totalSize;
-	}
-
-	public int getActualDataSize(int colType, Object data)
-	{
-		int size = 0;
-		switch (colType)
-		{
-		case Types.ARRAY:
-			break;
-		case Types.BOOLEAN:
-			size = 1;
-			break;
-		case Types.BINARY:
-			if (data != null && byte[].class.equals(data.getClass()))
-				size = ((byte[]) data).length;
-			else if (data != null && ByteArray.class.equals(data))
-				size = ((ByteArray) data).length();
-			break;
-		case Types.DATE:
-			size = 4;
-			break;
-		case Types.DOUBLE:
-			size = 8;
-			break;
-		case Types.BIGINT:
-			size = 8;
-			break;
-		case Types.NVARCHAR:
-			if (data != null && String.class.equals(data.getClass()))
-				size = ((String) data).getBytes().length;
-			break;
-		case Types.TIMESTAMP:
-			size = 12;
-			break;
-		}
-		return size;
-	}
-
-	public String getTableSpec(String catalog, String schema, String table)
-	{
-		String tableSpec = "";
-		if (catalog != null && !"".equals(catalog))
-			tableSpec = tableSpec + catalog + ".";
-		if (schema != null && !"".equals(schema) && !"public".equals(schema))
-			tableSpec = tableSpec + schema + ".";
-		tableSpec = tableSpec + table;
-
-		return tableSpec;
-	}
-
-	public static <T> List<List<T>> partition(final List<T> list, final int size)
-	{
-		if (list == null)
-		{
-			throw new NullPointerException("List must not be null");
-		}
-		if (size <= 0)
-		{
-			throw new IllegalArgumentException("Size must be greater than 0");
-		}
-		return new Partition<>(list, size);
-	}
-
-	/**
-	 * Provides a partition view on a {@link List}.
-	 * 
-	 * @since 4.0
-	 */
-	private static class Partition<T> extends AbstractList<List<T>>
-	{
-		private final List<T> list;
-		private final int size;
-
-		private Partition(final List<T> list, final int size)
-		{
-			this.list = list;
-			this.size = size;
-		}
-
-		@Override
-		public List<T> get(final int index)
-		{
-			final int listSize = size();
-			if (listSize < 0)
-			{
-				throw new IllegalArgumentException("negative size: " + listSize);
-			}
-			if (index < 0)
-			{
-				throw new IndexOutOfBoundsException("Index " + index + " must not be negative");
-			}
-			if (index >= listSize)
-			{
-				throw new IndexOutOfBoundsException("Index " + index + " must be less than size " + listSize);
-			}
-			final int start = index * size;
-			final int end = Math.min(start + size, list.size());
-			return list.subList(start, end);
-		}
-
-		@Override
-		public int size()
-		{
-			return (list.size() + size - 1) / size;
-		}
-
-		@Override
-		public boolean isEmpty()
-		{
-			return list.isEmpty();
-		}
 	}
 
 }
