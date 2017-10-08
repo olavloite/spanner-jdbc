a5c1fbec88c86de4886bed87c48cf062a6dce529
olavloite <koloite@gmail.com>
2017-10-08 21:16:11 +0200

    added repeatable read test

--- src/test/java/nl/topicus/jdbc/test/integration/TransactionTester.java
+++ src/test/java/nl/topicus/jdbc/test/integration/TransactionTester.java
@@ -2,6 +2,7 @@ package nl.topicus.jdbc.test.integration;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -11,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
+
+import nl.topicus.jdbc.CloudSpannerConnection;
 
 public class TransactionTester
 {
@@ -28,6 +31,7 @@ public class TransactionTester
 		runRollbackTests();
 		runReadOnlyTests();
 		runAutoCommitTests();
+		runRepeatableReadTest();
 	}
 
 	private void runAutoCommitTests() throws SQLException
@@ -35,7 +39,7 @@ public class TransactionTester
 		connection.commit();
 		connection.setAutoCommit(true);
 		List<Object[]> originalRows = getResultList("SELECT * FROM TEST");
-		insertRowInTest();
+		insertRowInTest(100000l);
 		List<Object[]> rows = getResultList("SELECT * FROM TEST");
 		connection.setAutoCommit(false);
 		Assert.assertEquals(originalRows.size() + 1, rows.size());
@@ -49,7 +53,7 @@ public class TransactionTester
 		List<Object[]> originalRows = getResultList("SELECT * FROM TEST");
 		try
 		{
-			insertRowInTest();
+			insertRowInTest(100000l);
 			connection.commit();
 		}
 		catch (SQLException e)
@@ -68,7 +72,7 @@ public class TransactionTester
 	private void runRollbackTests() throws SQLException
 	{
 		List<Object[]> originalRows = getResultList("SELECT * FROM TEST");
-		insertRowInTest();
+		insertRowInTest(100000l);
 		connection.rollback();
 		List<Object[]> rows = getResultList("SELECT * FROM TEST");
 		Assert.assertEquals(originalRows.size(), rows.size());
@@ -76,11 +80,41 @@ public class TransactionTester
 			Assert.assertArrayEquals(originalRows.get(index), rows.get(index));
 	}
 
-	private void insertRowInTest() throws SQLException
+	private void runRepeatableReadTest() throws SQLException
+	{
+		// Do a read on the original connection. This starts a transaction.
+		List<Object[]> originalRows = getResultList("SELECT * FROM TEST");
+
+		// Open a new connection and do an insert. This also starts a different
+		// transaction.
+		CloudSpannerConnection csConnection = (CloudSpannerConnection) connection;
+		Connection otherConnection = DriverManager.getConnection(csConnection.getUrl(),
+				csConnection.getSuppliedProperties());
+		otherConnection.setAutoCommit(false);
+		insertRowInTest(100001l);
+		// Commit the changes
+		otherConnection.commit();
+		otherConnection.close();
+
+		// Do a read on the original connection. This read should be equal to
+		// the previous read.
+		List<Object[]> rows = getResultList("SELECT * FROM TEST");
+		Assert.assertEquals(originalRows.size(), rows.size());
+		for (int index = 0; index < rows.size(); index++)
+			Assert.assertArrayEquals(originalRows.get(index), rows.get(index));
+
+		// Do a commit and then do the same query. Now the new row should be
+		// found.
+		connection.commit();
+		List<Object[]> newRows = getResultList("SELECT * FROM TEST");
+		Assert.assertEquals(originalRows.size() + 1, newRows.size());
+	}
+
+	private void insertRowInTest(long id) throws SQLException
 	{
 		PreparedStatement ps = connection.prepareStatement(
 				"INSERT INTO TEST (ID, UUID, ACTIVE, AMOUNT, DESCRIPTION, CREATED_DATE, LAST_UPDATED) VALUES (?, ?, ?, ?, ?, ?, ?)");
-		ps.setLong(1, 100000l);
+		ps.setLong(1, id);
 		ps.setBytes(2, "FOO".getBytes());
 		ps.setBoolean(3, true);
 		ps.setDouble(4, 50d);
