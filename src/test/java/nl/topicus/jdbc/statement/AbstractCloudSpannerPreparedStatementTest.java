f55c100a561ded6009a9f4957c8d9f6151e9989f
olavloite <koloite@gmail.com>
2017-10-09 09:46:38 +0200

    added test classes for completeness

---
+++ src/test/java/nl/topicus/jdbc/statement/AbstractCloudSpannerPreparedStatementTest.java
@@ -0,0 +1,12 @@
+package nl.topicus.jdbc.statement;
+
+import org.junit.experimental.categories.Category;
+
+import nl.topicus.jdbc.test.category.UnitTest;
+
+@Category(UnitTest.class)
+public class AbstractCloudSpannerPreparedStatementTest
+{
+	// All tests in CloudSpannerPreparedStatementTest
+
+}
