8293e7229c2120abd5a51d94b2a4c209535d04c0
Olav Loite <koloite@gmail.com>
2018-02-03 07:47:23 +0100

    added documentation to explain the use of SQLException

--- src/main/java/nl/topicus/jdbc/AbstractCloudSpannerFetcher.java
+++ src/main/java/nl/topicus/jdbc/AbstractCloudSpannerFetcher.java
@@ -10,24 +10,66 @@ public abstract class AbstractCloudSpannerFetcher extends AbstractCloudSpannerWr
 {
 	private int fetchSize = 1;
 
+	private int direction = ResultSet.FETCH_FORWARD;
+
+	/**
+	 * 
+	 * @param direction
+	 *            The fetch direction to use. Only
+	 *            {@link ResultSet#FETCH_FORWARD} is supported.
+	 * @throws SQLException
+	 *             Thrown if any other fetch direction than
+	 *             {@link ResultSet#FETCH_FORWARD} is supplied
+	 */
 	public void setFetchDirection(int direction) throws SQLException
 	{
 		if (direction != ResultSet.FETCH_FORWARD)
 			throw new SQLFeatureNotSupportedException("Only FETCH_FORWARD is supported");
+		this.direction = direction;
 	}
 
+	/**
+	 * 
+	 * @return Always returns {@link ResultSet#FETCH_FORWARD}
+	 * @throws SQLException
+	 *             Cannot be thrown by this method, but is added to the method
+	 *             signature in order to comply with the interfaces that will be
+	 *             implemented by this abstract class' concrete subclasses
+	 */
 	public int getFetchDirection() throws SQLException
 	{
-		return ResultSet.FETCH_FORWARD;
+		if (this.direction != ResultSet.FETCH_FORWARD)
+			throw new SQLFeatureNotSupportedException("Only FETCH_FORWARD is supported");
+		return this.direction;
 	}
 
+	/**
+	 * 
+	 * @param rows
+	 *            The number of rows to fetch
+	 * @throws SQLException
+	 *             Thrown if <code>rows&lt;1</code>
+	 * 
+	 */
 	public void setFetchSize(int rows) throws SQLException
 	{
+		if (rows < 1)
+			throw new SQLException("rows cannot be less than 1");
 		this.fetchSize = rows;
 	}
 
+	/**
+	 * 
+	 * @return The number of rows to fetch
+	 * @throws SQLException
+	 *             Cannot be thrown by this method, but is added to the method
+	 *             signature in order to comply with the interfaces that will be
+	 *             implemented by this abstract class' concrete subclasses
+	 */
 	public int getFetchSize() throws SQLException
 	{
+		if (fetchSize < 1)
+			throw new SQLException("fetchSize cannot be less than 1");
 		return fetchSize;
 	}
 
